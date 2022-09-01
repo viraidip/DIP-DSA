ef0ad452b03395cbfca6fb77efb707e2e27e7705
Jens Lohmann <jens.johann@outlook.de>
2022-09-01 21:09:13 +0200

    Added info to select dataset

--- src/ui/dataset.R
+++ src/ui/dataset.R
@@ -3,10 +3,16 @@ library(tools)
 all_datasets <- tools::file_path_sans_ext(list.files(file.path("..", "data", "datasets")))
 
 dataset_tab <- tabItem(tabName="dataset",
-  h1("Select/Load dataset"),
+  h1("Select an existing dataset or load a custom one"),
   fluidRow(
     box(
       title="Select dataset",
+      dropMenu(
+        dropdownButton("Info", status='success', icon=icon('info')),
+        h3(strong('Information')),
+        br(),
+        h5('This is really helpful'),
+      ),
       selectInput(
         inputId="strain",
         label="select an existing dataset:",
@@ -14,7 +20,7 @@ dataset_tab <- tabItem(tabName="dataset",
       ),
     ),
     box(
-      title="upload dataset",
+      title="Upload dataset",
       "Allows the user to upload a custom dataset",
       "Not only the dataset but also the full length sequences have to be uploaded",
       fileInput(
@@ -23,7 +29,7 @@ dataset_tab <- tabItem(tabName="dataset",
       )
     ),
     box(
-      title="overview",
+      title="Overview",
       width=12,
       "display a table with the data",
       dataTableOutput("dataset_table")
