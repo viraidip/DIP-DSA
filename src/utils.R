d92b4d2e1441565979db23d8c571c54c042e7499
Jens Lohmann <jens.johann@outlook.de>
2022-09-01 21:53:01 +0200

    Changed get_seq() to return RNA sequence and not DNA [T->U]

--- src/utils.R
+++ src/utils.R
@@ -5,7 +5,7 @@ SEGMENTS <- c("PB2", "PB1", "PA", "HA", "NP", "NA", "M", "NS")
 get_seq <- function(strain, segment) {
   path <- file.path("..", "data", "strain_segment_fastas", strain, paste(segment, ".fasta", sep=""))
   fasta <- readDNAStringSet(path)
-  return(fasta[[1]])
+  return(RNAString(fasta[[1]]))
 }
 
 get_seq_len <- function(strain, segment) {
