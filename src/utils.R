aca5affc85892ec0657a8b519bdfc9d429422ed0
Jens Lohmann <jens.johann@outlook.de>
2022-09-06 22:56:00 +0200

    Added prechecks before starting the application

--- src/utils.R
+++ src/utils.R
@@ -4,6 +4,13 @@ SEGMENTS <- c("PB2", "PB1", "PA", "HA", "NP", "NA", "M", "NS")
 
 DATASETSPATH <- file.path("..", "data", "datasets")
 FASTAPATH <- file.path("..", "data", "strain_segment_fastas")
+TEMPPATH <- file.path("..", "data", "temp")
+
+run_prechecks <- function() {
+  if (!file.exists(TEMPPATH)) {
+    dir.create(TEMPPATH)
+  }
+}
 
 get_seq <- function(strain, segment) {
   path <- file.path(FASTAPATH, strain, paste(segment, ".fasta", sep=""))
@@ -14,3 +21,4 @@ get_seq <- function(strain, segment) {
 get_seq_len <- function(strain, segment) {
   return(length(get_seq(strain, segment)))
 }
+
