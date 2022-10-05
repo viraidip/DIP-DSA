45e3bf2356f277811eb6fd370d96070ea89f840d
Jens Lohmann <jens.johann@outlook.de>
2022-10-05 21:07:56 +0200

    Changed coloring of nucleotides

--- src/utils.R
+++ src/utils.R
@@ -8,7 +8,7 @@ DATASETSPATH <- file.path(DATAPATH, "datasets")
 FASTAPATH <- file.path(DATAPATH, "strain_segment_fastas")
 TEMPPATH <- file.path(DATAPATH, "temp")
 
-COLOR_MAP <- hash(A="blue", C="green", G="yellow", U="red")
+COLOR_MAP <- hash(A="blue", C="yellow", G="green", U="red")
 NUC_MAP <- hash(A="Adenin", C="Cytosin", G="Guanin", U="Uracil")
 
 run_prechecks <- function() {
