e670bf3c613ea350baf910d42699ecc2353c25b3
Jens Lohmann <jens.johann@outlook.de>
2022-11-24 14:30:01 +0100

    Including text

--- src/server/dataset.R
+++ src/server/dataset.R
@@ -2,6 +2,8 @@
 generate_stats_info <- function(df) {
   return(
     paste(
+      "NGS count:",
+      "##########",
       paste("Mean:", mean(df$NGS_read_count)),
       paste("Median:", median(df$NGS_read_count)),
       paste("Max:", max(df$NGS_read_count)),
