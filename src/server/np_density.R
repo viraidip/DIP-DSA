73bb09ec6c88f77e12b89f5992016e88edd9727c
Jens Lohmann <jens.johann@outlook.de>
2022-12-15 12:32:08 +0100

    Created first draft of plot that compares observed and expected ratio of high/low areas

--- src/server/np_density.R
+++ src/server/np_density.R
@@ -26,33 +26,51 @@ in_high_np_area <- function(row, a_df) {
   for (i in 1:nrow(a_df)) {
     s <- a_df[i, "start"]
     e <- a_df[i, "end"]
-    if ((p >= s) && (p <= e)) {
+    if (p >= s && p <= e) {
       return("high")
     }
   }
   return("low")
 }
 
+count_area_occurrence <- function(df, a_df, label) {
+  df["NP_area"] <- apply(df, 1, in_high_np_area, a_df=a_df)
+  table_df <- data.frame(table(df$NP_area))
+  # dividing high/low
+  final_df <- data.frame(Var1="Ratio", Freq=table_df[1, 2]/table_df[2, 2])
+  final_df["Class"] <- label
+  return(final_df)
+}
+
 create_np_bar_plot <- function(df, strain, segment, areas) {
   # reformat data
   obs_df <- format_dataframe_locations(df, segment, "flattened")
-  a_df <- reformat_np_areas(areas)
 
+  # create sampling data
   seq <- get_seq(strain, segment)
   n_samples <- nrow(df) * 5
   sam_df <- create_direct_repeat_sampling_data(df, n_samples, seq)
   sam_df["Segment"] <- rep(segment, n_samples)
   sam_df <- format_dataframe_locations(sam_df, segment, "flattened")
 
-  obs_df["np_area"] <- apply(obs_df, 1, in_high_np_area, a_df=a_df)
-  sam_df["np_area"] <- apply(sam_df, 1, in_high_np_area, a_df=a_df)
+  # reformat np areas, if they are valid areas provided create a plot
+  a_df <- reformat_np_areas(areas)
+  if (nrow(a_df) == 0) {
+    p <- ggplot()
+  } else {
+    # count occurrences inside and outside of NP areas
+    o_df <- count_area_occurrence(obs_df, a_df, "observed")
+    s_df <- count_area_occurrence(sam_df, a_df, "expected")
+    plot_df <- rbind(o_df, s_df)
 
-  # create plot
-  p <- ggplot(df, aes(x=Position, y=NGS_read_count, fill=Class)) +
-    geom_bar(stat="identity") +
-    xlab("Nucleotide position on segment") +
-    ylab("NGS read count")
+    # TODO: add statistical comparision
 
+    # create plot
+    p <- ggplot(data=plot_df, aes(x=Var1, y=Freq, fill=Class)) +
+      geom_bar(stat="identity", position=position_dodge()) +
+      xlab("Nucleotide position on segment") +
+      ylab("NGS read count")
+  }
 
   ggplotly(p)
 }
--- src/ui/np_density.R
+++ src/ui/np_density.R
@@ -2,23 +2,24 @@ np_density_tab <- tabItem(tabName="np_density",
   h1("Nucleoprotein density"),
   fluidRow(
     box(
-      title="Input",
+      title="Input NP high areas",
       width=12,
-      "NP areas need to be defined in a single string. Each area is split by",
-      "a comma and the start and end of each area is seperated by a hyphen",
-      "(e.g. '10-40,60-85').",
+      "NP areas need to be defined as a string. The start and end point of an",
+      "area is split by a hyphen. To define multiple NP high areas seperate","
+      them by a comma (e.g. '10-40,60-85').",
       textInput(
         inputId="np_areas",
-        label="Write the areas of high NP density"
+        label="Write areas high NP density",
+        placeholder="45-178,406-524,1676-1894"
       )
     ),
     box(
-      title="Input",
+      title="Mapping of NP high areas to deletion sites",
       width=12,
       plotlyOutput("np_plot")
     ),
     box(
-      title="Input",
+      title="Comparision of distribution",
       width=12,
       plotlyOutput("np_bar_plot")
     )
