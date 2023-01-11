dcc46e340a1d9bc4ea245cec160ad674679d4ec9
Jens Lohmann <jens.johann@outlook.de>
2023-01-11 10:55:11 +0100

    Fixed typos

--- src/ui/dataset.R
+++ src/ui/dataset.R
@@ -4,13 +4,13 @@ dataset_tab <- tabItem(tabName="dataset",
     box(
       title="Statistical overview",
       width=12,
-      "Different statistical parameters for the NGS count of the dataset",
+      "Different statistical parameters for the NGS count of the dataset.",
       verbatimTextOutput("dataset_stats_info")
     ),
     box(
       title="Inspecting single entries",
       width=12,
-      "Entries can be selected by clicking on them. Further information can",
+      "Entries can be selected by clicking on them. Further information can.",
       "then be seen in the",
       actionLink("link_to_single_datapoint_tab", "Inspect single datapoint"),
       " tab",
