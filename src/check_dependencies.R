50d3d00524cbd0a6c6a0967dfd37c8467bbe4257
Jens Lohmann <jens.johann@outlook.de>
2023-02-09 12:06:51 +0100

    Fixed bug when installing libraries

--- src/check_dependencies.R
+++ src/check_dependencies.R
@@ -1,7 +1,11 @@
 
-packages = list("shiny", "shinydashboard", "ggplot2", "DT", "comprehenr", "hash", "plotly", "plyr", "jsonlite", "tools")
+pcks <- list("shiny", "shinydashboard", "ggplot2", "DT", "comprehenr", "hash", "plotly", "plyr", "jsonlite", "tools")
 
-install.packages(setdiff(packages, rownames(installed.packages())))
+missing_pcks <- setdiff(pcks, rownames(installed.packages()))
+
+for (m_pck in missing_pcks) {
+  install.packages(m_pck)
+}
 
 if (!require("Biostrings")) {
   if (!require("BiocManager", quietly = TRUE)) {
