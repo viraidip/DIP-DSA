80941809af16adb3c8f08f688566efb244f7ce8e
Jens Lohmann <jens.johann@outlook.de>
2023-02-23 12:21:28 +0100

    Created .yml file and create env from it

--- setup_script.sh
+++ setup_script.sh
@@ -1,6 +1,6 @@
 Rscript src/check_dependencies.R
 
 # load conda env and set path to it
-#conda create
-
+conda env create -f env.yml
 conda info --envs | grep -Po "dipdsa\K.*" | sed 's: ::g' > conda_path.txt
+
