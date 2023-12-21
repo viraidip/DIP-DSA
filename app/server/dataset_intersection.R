76576c5be54e8dc52078fea92fbf73579acd0d93
LohmannJens <jens.johann@outlook.de>
2023-12-21 11:53:39 +0100

    New version of dataset intersection tab

--- app/server/dataset_intersection.R
+++ app/server/dataset_intersection.R
@@ -1,92 +1,106 @@
-
-load_dataset <- function(fname) {
-  path <- file.path(DATASETSPATH, fname)
-  names <- c("Segment", "Start", "End", "NGS_read_count")
-  cl <- c("character", "integer", "integer", "integer")
-  if (file.exists(path)) {
-    df <- read.csv(path, na.strings=c("NaN"), col.names=names, colClasses=cl)
-  } else {
-    df <- data.frame(
-      "Segment"=character(),
-      "Start"=integer(),
-      "End"=integer(),
-      "NGS_read_count"=integer()
-    )
-  }
-  return(df)
-}
-
-all_intersecting_candidates <- function(datasets, thresh) {
+get_intersecting_candidates <- function(df) {
   candidates <- c()
-  for (p in datasets) {
-    df <- load_dataset(p)
-    cand <- paste(df$Segment, df$Start, df$End, sep="_")
+  for (name in unique(df$name)) {
+    n_df <- df[df$name == name, ]
+    cand <- paste(n_df$Segment, n_df$Start, n_df$End, sep="_")
     candidates <- append(candidates, cand)
   }
+
   counts <- table(candidates)
-  selected <- counts[counts >= thresh]
-  return(data.frame(selected))
+  final_df <- data.frame(counts)
+  colnames(final_df) <- c("DVG candidate", "occurrence")
+  return(final_df)
 }
 
-plot_overlap_matrix <- function(paths) {
+intersecting_candidates_table <- function(paths, RSC, thresh) {
+  df <- load_all_datasets(paths)
+  df <- apply_cutoff(df, RSC)
+
+  final_df <- get_intersecting_candidates(df)
+  final_df <- final_df[final_df$occurrence >= thresh, ]
+  return(final_df)
+}
+
+
+plot_overlap_matrix <- function(paths, RSC) {
   validation_text <- "No plot could be created. Please select at least two datasets."
   shiny::validate(need((length(paths) > 1), validation_text))
+
+  df <- load_all_datasets(paths)
+  df <- apply_cutoff(df, RSC)
   
   lists <- c()
-  for (p in paths) {
-    df <- load_dataset(p)
-    DI_list <- paste(df$Segment, df$Start, df$End, sep="_")
+  labels <- c()
+  for (name in unique(df$name)) {
+    n_df <- df[df$name == name, ]
+    DI_list <- paste(n_df$Segment, n_df$Start, n_df$End, sep="_")
     lists <- c(lists, list(DI_list))
+    labels <- c(labels, name)
   }
 
-  # initialize an empty matrix
+  # initialize an empty matrix, calculate fraciton and populate the matrix
   matrix_size <- length(lists)
   matrix <- matrix(0, nrow = matrix_size, ncol = matrix_size)
-
-  # calculate the differences and populate the matrix
   for (i in 1:matrix_size) {
+    set1 <- unique(lists[[i]])
     for (j in 1:matrix_size) {
-      set1 <- unique(lists[[i]])
       set2 <- unique(lists[[j]])
-
-      matrix[i, j] <- length(intersect(set1, set2)) / (max(length(set1), length(set2)))
+      matrix[i, j] <- length(intersect(set1, set2)) / (max(length(set1), length(set2))) * 100
     }
   }
-  
-  split_paths <- strsplit(paths, "/")
-  labels <- lapply(split_paths, function(path) {
-    last_part <- tail(path, 1)
-    cleaned_path <- gsub(".csv$", "", last_part)
-    return(cleaned_path)
-  })
 
-  df <- as.data.frame(as.table(matrix))
-  p <- ggplot(df, aes(x = Var1, y = Var2, fill=Freq)) +
+  plot_df <- as.data.frame(as.table(matrix))
+  colnames(plot_df) <- c("Dataset1", "Dataset2", "intersection")
+  p <- ggplot(plot_df, aes(x=Dataset1, y=Dataset2, fill=intersection)) +
     geom_tile() +
-    scale_fill_gradient(low = "white", high = "blue") +
-    labs(x = "Column", y = "Row", title = "Matrix Heatmap") +
+    scale_fill_gradient(low="white", high="blue") +
+    labs(x="Dataset", y="Dataset", fill="Intersection [%]") +
     scale_x_discrete(labels=labels) +
     scale_y_discrete(labels=labels)
 
   return(p)
 }
 
-plot_upset_plot <- function(paths) {
+
+plot_upset <- function(paths, RSC) {
   validation_text <- "No plot could be created. Please select at least two datasets."
   shiny::validate(need((length(paths) > 1), validation_text))
 
+  df <- load_all_datasets(paths)
+  df <- apply_cutoff(df, RSC)
+
   lists <- c()
   names <- c()
-  for (p in paths) {
-    df <- load_dataset(p)
-    DI_list <- paste(df$Segment, df$Start, df$End, sep="_")
-    name <- str_sub(str_extract(p, "/(.*?)\\."), 2, -2)
+  for (name in unique(df$name)) {
+    n_df <- df[df$name == name, ]
+    DI_list <- paste(n_df$Segment, n_df$Start, n_df$End, sep="_")
+    lists <- c(lists, list(DI_list))
     names <- c(names, name)
-    lists <- c(lists, name=list(DI_list))
   }
   names(lists) <- names
 
   m = make_comb_mat(lists)
   p <- UpSet(m)
   return(p)
+}
+
+
+plot_intersecting_candidates_NGS <- function(paths, RSC) {
+  df <- load_all_datasets(paths)
+  df <- apply_cutoff(df, RSC)
+
+  c_df <- get_intersecting_candidates(df)
+  thresh <- max(c_df$occurrence)
+  sel_df <- c_df[c_df$occurrence >= thresh, ]
+  
+  df$key <- paste(df$Segment, df$Start, df$End, sep="_")
+  marked_points <- df[df$key %in% as.character(sel_df[["DVG candidate"]]), ]
+
+  pl <- ggplot(df, aes(x=name, y=NGS_read_count, color=name)) +
+    geom_boxplot() +
+    scale_y_log10() +
+    labs(x="Dataset", y="NGS count (log scale)", color="Legend", fill="") +
+    geom_point(data=marked_points, aes(fill=key), size=3, shape=8)
+
+  ggplotly(pl)
 }
--- app/ui/dataset_intersection.R
+++ app/ui/dataset_intersection.R
@@ -16,8 +16,8 @@ dataset_intersection_tab <- tabItem(tabName="dataset_intersection",
         selected=c("A_California_07_2009/Alnaji2019_Cal07.csv", "A_NewCaledonia_1999/Alnaji2019_NC.csv")
       ),
       sliderInput(
-        inputId="RCS_intersection",
-        label="Set RCS:",
+        inputId="RSC_intersection",
+        label="Set the RSC (read support cutoff)",
         1,
         100,
         2,
@@ -35,10 +35,14 @@ dataset_intersection_tab <- tabItem(tabName="dataset_intersection",
         ),
         2,
         step=1
+      ),
+      actionButton(
+        inputId="intersection_submit",
+        label="Generate plots"
       )
     ),
     box(
-      title="table",
+      title="DVG candidates above defined threshold",
       width=12,
       dataTableOutput("intersecting_candidates_table")
     ),
@@ -51,6 +55,11 @@ dataset_intersection_tab <- tabItem(tabName="dataset_intersection",
       title="Upset plot",
       width=12,
       plotOutput("upset_plot")
+    ),
+    box(
+      title="Identified candidates with NGS counts",
+      width=12,
+      plotlyOutput("intersecting_candidates_NGS_plot")
     )
   )
 )
