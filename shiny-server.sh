67803a701fa563712356e6c1677be8fed569da36
LohmannJens <jens_lohmann@outlook.de>
2024-04-18 16:43:02 +0200

    Updated setup on server script

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,4 +1,4 @@
 export IMAGE="dip-dsa_r-shiny_app:latest"
 export FILE="Dockerfile"
 DOCKER_BUILDKIT=1 docker build --no-cache -f $FILE -t $IMAGE .
-docker run -p 8161:8161 $IMAGE
+docker run -p 8161:8161 -d --restart always $IMAGE
