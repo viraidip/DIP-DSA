dafc11ea2a61f830f2c7a92ba44f57489729a78f
LohmannJens <jens_lohmann@outlook.de>
2024-05-27 11:55:05 +0200

    Added handling of old image and container

--- shiny-server.sh
+++ shiny-server.sh
@@ -1,4 +1,9 @@
 export IMAGE="dip-dsa_r-shiny_app:latest"
 export FILE="Dockerfile"
+
+# stop old container
+docker ps --format "{{.ID}}" --filter "ancestor="${IMAGE} | xargs docker stop
+docker rmi -f $IMAGE
+
 DOCKER_BUILDKIT=1 docker build --no-cache -f $FILE -t $IMAGE .
-docker run -p 8161:8161 -d --restart always $IMAGE
+docker run -p 8161:8161 -d --name "dip-dsa_r-shiny_app" --restart always $IMAGE
